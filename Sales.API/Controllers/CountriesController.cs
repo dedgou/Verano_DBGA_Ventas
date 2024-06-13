413bacc8218628a6166db8af94fd1019fc24d216
Diego Brandon García Avendaño <20030091@itesa.edu.mx>
2024-06-13 12:55:56 -0600

    Version03. Controladores de la API para habilitar su consumo.

---
+++ Sales.API/Controllers/CountriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("api/countries")]
+    public class CountriesController:ControllerBase
+    {
+        private readonly DataContext _context;
+        
+        public CountriesController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country0000000000000000000);
+        }
+    }
+}
