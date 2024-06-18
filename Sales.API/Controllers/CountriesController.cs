ed0204f415eb814a006e0dfc54de5fc1808589bc
Diego Brandon García Avendaño <20030091@itesa.edu.mx>
2024-06-18 10:00:45 -0600

    Versión05. Controladores de la API completos.

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -5,29 +5,67 @@ using Sales.Shared.Entities;
 
 namespace Sales.API.Controllers
 {
+
     [ApiController]
-    [Route("api/countries")]
-    public class CountriesController:ControllerBase
+    [Route("/api/countries")]
+    public class CountriesController : ControllerBase
     {
         private readonly DataContext _context;
-        
         public CountriesController(DataContext context)
         {
             this._context = context;
         }
 
+        //Método post
         [HttpPost]
         public async Task<ActionResult> PostAsync(Country country)
         {
-            _context.Add(country);
+            _context.Countries.Add(country);
             await _context.SaveChangesAsync();
             return Ok(country);
         }
 
+        //Método get
         [HttpGet]
         public async Task<IActionResult> GetAsync()
         {
             return Ok(await _context.Countries.ToListAsync());
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        //Método put
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        //Método delete 
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+
+                return NotFound();
+
+            }
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
